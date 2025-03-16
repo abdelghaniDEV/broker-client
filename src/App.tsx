f26c06cb88c4ca8c53415257c55aa8b485faac9d
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2025-03-16 03:56:08 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import "./App.css";
 import { MarketOverview } from "react-ts-tradingview-widgets";
 import { Button } from "./components/ui/button";
 import TradingViewWidget from "./components/TradingViewWidget";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const token  = localStorage.getItem('token-001')
-    // if(!token) {
-    //   window.location.href = import.meta.env.VITE_LANDING_URL
-    // }
-  },[])
+  // useEffect(() => {
+  //   const token  = localStorage.getItem('token-001')
+  //   if(!token) {
+  //     window.location.href = import.meta.env.VITE_LANDING_URL
+  //   }
+  // },[])
   return (
     <div className="flex flex-col md:flex-row bg-black ">
       <TradingViewWidget />
